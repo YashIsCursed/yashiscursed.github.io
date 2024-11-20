db2e31d3fdca1a923d7486aa50575b5b742f756e
yashbokade1190 <yashbokade1190@gmail.com>
2024-11-20 15:43:35 +0000

    initial adding info

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--bg)",
+        foreground: "var(--fg)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
       },
     },
   },
